1cd1779560afddafe93cdb03ecbdf63a2bcb8333
Anthony de Hoog <37657@ma-web.nl>
2023-10-09 12:35:03 +0200

    haikumamatata

--- haiku.js
+++ haiku.js
@@ -8,10 +8,10 @@ function Haiku1(){
     console.log("mama, 'kwil naar huis");
 }
 function Haiku2(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+    console.log("haiku by:","Anthony");
+    console.log("They call me Sokka");
+    console.log("That is in the watertribe");
+    console.log("I'm not a oaf");
 }
 function Haiku3(){
     console.log("haiku by:","AUTHOR");
