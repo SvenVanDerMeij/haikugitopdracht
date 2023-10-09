8226c9923eb7512c82ced55a51ec24d2335166bf
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-10-09 12:30:08 +0200

    haiku

--- haiku.js
+++ haiku.js
@@ -2,10 +2,10 @@
 
 //plak hier de haiku functions onder elkaar
 function Haiku1(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+    console.log("haiku by:","Sven");
+    console.log("ik wil niet meer zijn");
+    console.log("github is kut leven ook");
+    console.log("mama, 'kwil naar huis");
 }
 function Haiku2(){
     console.log("haiku by:","AUTHOR");
