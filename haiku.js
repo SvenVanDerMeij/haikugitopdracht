5c85ca20343b599bb229dbf521b3598abdc0e80f
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-10-09 12:23:35 +0200

    ahahahahhaha

--- haiku.js
+++ haiku.js
@@ -7,11 +7,17 @@ function HaikuX(){
     console.log("REGEL2");
     console.log("REGEL3");
 }
-function Haiku1(){
-    console.log("haiku by:","Docent");
-    console.log("Panda in the mist");
-    console.log("Gently chewing on bamboo");
-    console.log("Perfect breakfast time");
+function Haiku2(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function Haiku3(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
 }
 
 
