877e365310b5989223aa063439bcbb7cd9e9ff81
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-10-09 12:21:58 +0200

    help

--- haiku.js
+++ haiku.js
@@ -1,7 +1,18 @@
 
 
 //plak hier de haiku functions onder elkaar
-
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function Haiku1(){
+    console.log("haiku by:","Docent");
+    console.log("Panda in the mist");
+    console.log("Gently chewing on bamboo");
+    console.log("Perfect breakfast time");
+}
 
 
 
